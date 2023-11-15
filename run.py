8f723325ef3a5267a0f47153bb7df34120e7f78f
fekadon <>
2023-11-15 16:09:24 +0000

    Create new_grid and lookGreat functions. Used to get a graphic of a grid and make it look nicer. Also added both to main function and both works

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 Tic Tac Toe
 """
 
+
 """
 Introduktion with the rules of Tic Tac Toe
 """
@@ -13,12 +14,43 @@ def intro():
           "The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.")
     print("\n")
     input("Press enter to continue!")
+    print("\n")
+
+
+"""
+This creates a empty grid
+"""
+def new_grid():
+    print("The new board:")
+    grid = [[" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]]
+    return grid
+
+
+"""
+This change look on the grid and makes it nicer
+"""
+def lookGreat(grid):
+    rows = len(grid)
+    cols = len(grid)
+    print("-----------")
+    for r in range(rows):
+        print(grid[r][0], " |", grid[r][1], "|", grid[r][2])
+        print("-----------")
+    return grid
+
 
 """
 Main function, calls all other functions
 """
 def main():
     introduktion = intro()
+    grid = new_grid()
+    nicer = lookGreat(grid)
 
 
+"""
+Call main function
+"""
 main()
